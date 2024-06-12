4546bac09db2ddf19249309c169c9589c937290d
Taifinkaba <taifinkabad@gmail.com>
2024-06-12 17:06:24 -0400

    section 1 done

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,13 +1,34 @@
 const userUrl = 'https://jsonplaceholder.typicode.com/users'
 
 export const checkResponseStatus = () => {
+   return fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        return {
+        status: response.status,
+        ok:  response.ok,
+        url: response.url
+        };
+    })
 };
 
 export const getUsers = () => {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+    .then((resp) => resp.json())
 };
 
-export const getUserPosts = () => {
+export const getUserPosts = (userId) => {
+    return fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+    .then((resp) => resp.json())
+    .then(post => post)
 };
 
-export const createNewUser = () => {
+export const createNewUser = (newUserData) => {
+    const options = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newUserData)
+    }
+    return fetch('https://jsonplaceholder.typicode.com/users', options)
+    .then((response) => response.json())
+
 }
